aca4ef3b461f0b1bfd22cb51457625879db2a8b9
Stasinos Konstantopoulos <stasinos@konstant.gr>
2023-05-29 21:09:21 +0300

    Defined the learning language

--- math_expr/README.md
+++ math_expr/README.md
@@ -1,10 +1,37 @@
 # Mathematical Expression Construction
 
-Machine learning exercise on learning mathematical
-expressions by obsering graphs. The objective is
-to construct the mathematical expression that best
-matches a graph. The most "elegant" solution must
-be found, so simply approximating with a polyonym
-is not considered a good solution.
+Machine learning exercise on learning mathematical expressions by
+observing graphs. The objective is to construct the mathematical
+expression that when plotted, best matches the observed graph. The
+most "elegant" solution must be found, so simply approximating with a
+polyonym is not considered a good solution.
 
+For the sake of simplicity, we shall restrict the scope of the
+exercise to learning a "program" in a "programming language" that has
+the following functions:
 
+ - `exp(a)`: f(x) = a*exp(x), with real argument a
+
+ - `sin(a,b)`: f(x) = a*sin(b*x), with real arguments a,b
+
+ - `sinc(x0)`: f(x) = sinc(x-x0), with real argument x0
+
+ - `poly(A)`: f(x) = the polynomial Sum[i in 0..len(A)-1]( A[i]*x^i ),
+   with an array of integers as argument.
+ 
+A "program" consists of multiple lines, where each line is exactly one
+function call. The semantics of each line is the expression given
+above. The semantics of the whole program is the graph for y=f(x),
+for x in [-10,10], where f(x) is the summation of the semantics of
+all lines.
+
+For example, the following program:
+
+```
+sinc( -2 )
+poly( [0,0.2] )
+```
+
+means the graph plotted by the equation
+y = sinc(x) + 0.2*x
+for x in [-10,10].
--- math_expr/generator.py
+++ math_expr/generator.py
@@ -2,6 +2,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
+def fn_exp( a, xtrans=0, ytrans=0 ):
+    x = np.arange(-10, 10, 0.001)
+    y = (ytrans) + a*np.exp(x-xtrans)
+    return x,y
+    
+def fn_ln( xtrans=0, ytrans=0 ):
+    x = np.arange(0.01, 10, 0.001)
+    y = (ytrans) + np.log(x-xtrans)
+    return x,y
+
+def fn_sin( a, b, xtrans=0, ytrans=0 ):
+    x = np.arange(-10, 10, 0.001)
+    y = (ytrans) + a*np.sin(b*x-xtrans)
+    return x,y
+
+def fn_sinc( xtrans=0, ytrans=0 ):
+    x = np.arange(-10, 10, 0.001)
+    y = (ytrans) + np.sinc(x-xtrans)
+    return x,y
+
+def fn_poly( A, xtrans=0 ):
+    x = np.arange(-10, 10, 0.001)
+    y = np.zeros(len(x))
+    for i,xi in enumerate(x):
+        for j,a in enumerate(A):
+            y[i] += a*(xi**j)
+    return x,y
+
 
 def write_figure( title, x, y, xlim, ylim, pure=False ):
     plt.xlim(xlim)
@@ -32,50 +60,36 @@ def write_figure( title, x, y, xlim, ylim, pure=False ):
 
 xlim = [-10,10]
 ylim = [-5,5]
-pure = True
+pure = False#True
 #xtrans = -2
 #ytrans = -0.5
 xtrans = 0
 ytrans = 0
 
 x = np.arange(-11, 11, 0.1)
-a = 0.2
-b = 0.3
-c = 0.1
+a = 0.0002
+b = 0.0003
+c = 0.0001
 y = (ytrans) + a*(x-xtrans)**3 + b*(x-xtrans)**2 + c*(x-xtrans)
 write_figure( "poly", x, y, xlim, ylim, pure )
 
-x = np.arange(0.2, 11, 0.001)
-y = (ytrans) + np.log(x-xtrans)
+x,y = fn_ln()
 write_figure( "natlog", x, y, xlim, ylim, pure )
 
-x = np.arange(-10, 10, 0.01)
-a = 0.03
-y = (ytrans) + a*np.exp(x-xtrans)
+x,y = fn_exp( 0.03 )
 write_figure( "exp", x, y, xlim, ylim, pure )
 
-x = np.arange(-11, 11, 0.001)
-a = 2
-b = 3
-y = (ytrans) + a*np.sin(b*x-xtrans)
+x,y = fn_sin( 2, 3 )
 write_figure( "sine", x, y, xlim, ylim, pure )
 
-x = np.arange(-11, 11, 0.01)
-y = (ytrans) + np.sinc(x-xtrans)
+x,y = fn_sinc()
 write_figure( "sinc", x, y, xlim, ylim, pure )
 
-x = np.arange(-11, 11, 0.001)
-y = (ytrans) + np.cosh(x-xtrans)
-write_figure( "cosh", x, y, xlim, ylim, pure )
+x,y1 = fn_sinc(-2)
+x,y2 = fn_poly([0,0.2])
+write_figure( "sinc_linear", x, y1+y2, xlim, ylim, pure )
 
-x = np.arange(-11, 11, 0.01)
-c = 0.2
-y = (ytrans) + np.sinc(x-xtrans) + c*x
-write_figure( "sinconlinear", x, y, xlim, ylim, pure )
-
-x = np.arange(0.2, 11, 0.001)
-a = 0.1
-b = 10
-y = (ytrans) + np.log(x-xtrans) + a*np.sin(b*x-xtrans)
-write_figure( "sineonln", x, y, xlim, ylim, pure )
+x,y1 = fn_poly([0,0.1,0.3,0.2])
+x,y2 = fn_sin(5,10)
+write_figure( "sin_npoly", x, y1+y2, xlim, ylim, pure )
 
