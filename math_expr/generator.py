59b87c9a421606d9cc922bff15ef219c46def566
Stasinos Konstantopoulos <stasinos@konstant.gr>
2023-05-30 19:19:13 +0300

    Parse individual lines

--- math_expr/generator.py
+++ math_expr/generator.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+import json
 
 def fn_exp( x=None ):
     if x is None:
@@ -53,6 +54,55 @@ def op_compose( fn_list, x=None ):
                 assert 1==2
     return x,y
 
+def handle_fn_call( fn, param, x=None ):
+    y = None
+    if   fn == "exp":  _,y = fn_exp(x=x)
+    elif fn == "ln":   _,y = fn_ln(x=x)
+    elif fn == "sin":  _,y = fn_sin(x=x)
+    elif fn == "sinc": _,y = fn_sinc(x=x)
+    elif fn == "poly":
+        A = json.loads(param)
+        _,y = fn_poly(A,x=x)
+    else:
+        print(fn)
+        assert 1 == 2
+    return y
+
+def handle_composition( line, x=None ):
+    functions = {}
+    parameters= {}
+    calls = []
+    fn_names = ["exp","ln","sinc","sin","poly"]
+    for fn_name in fn_names:
+        index = line.rfind( fn_name )
+        while (index > -1):
+            if index not in functions.keys():
+                # check for prior keys to avoid re-adding sin where sinc was found
+                functions[index] = fn_name
+            index = line.rfind( fn_name, 0, index )
+    for k,v in sorted(functions.items()):
+        calls.append( (k,v) )
+    prev_index = None
+    prev_fn_name = None
+    calls1 = []
+    for (index,fn_name) in calls:
+        if prev_index is not None:
+            prev_params1 = line.rfind( "(", 0, index )
+            prev_params2 = line.rfind( ")", 0, index )
+            prev_params = line[prev_params1+1:prev_params2]
+            calls1.append( (prev_fn_name,prev_params) )
+        prev_index = index
+        prev_fn_name = fn_name
+    last_params1 = line.rfind( "(" )
+    last_params2 = line.rfind( ")" )
+    last_params = line[last_params1+1:last_params2]
+    calls1.append( (prev_fn_name,last_params) ) 
+    if x is None: x = np.arange(-10, 10, 0.01)
+    y = x
+    for (fn_name,params) in reversed(calls1):
+        y = handle_fn_call( fn_name, params, y )
+    return x,y
+
 def write_figure( title, x, y, xlim, ylim, pure=False ):
     plt.xlim(xlim)
     plt.ylim(ylim)
@@ -81,49 +131,55 @@ def write_figure( title, x, y, xlim, ylim, pure=False ):
     pic = Image.open("{}.png".format(title)).convert('1')
     pic.save( "{}-bw.png".format(title) )
 
+def execute( program ):
+    file = open( program )
+    lines = file.readlines()
+    for line in lines:
+        pass
+    file.close()
 
 xlim = [-10,10]
 ylim = [-5,5]
 pure = True
 
-x,y = fn_poly( [0,0.001,0.003,0.002] )
+x,y = handle_composition( "poly([0,0.001,0.003,0.002])" )
 write_figure( "poly", x, y, xlim, ylim, pure )
 
-x,y = fn_ln()
+x,y = handle_composition( "ln()" )
 write_figure( "natlog", x, y, xlim, ylim, pure )
 
-x,y = fn_exp()
+x,y = handle_composition( "exp ( )" )
 write_figure( "exp", x, y, xlim, ylim, pure )
 
-x,y = op_compose( [("fn_poly",[0,2]),"fn_sin",("fn_poly",[0,3])] )
+x,y = handle_composition( "poly( [0,2]),sin(),poly([0,3])" )
 write_figure( "sine", x, y, xlim, ylim, pure )
 
-x,y = fn_sinc()
+x,y = handle_composition( "sinc ()" )
 write_figure( "sinc", x, y, xlim, ylim, pure )
 
-x,y1 = op_compose( ["fn_sinc",("fn_poly",[2,1])] )
-x,y2 = fn_poly([0,0.2])
+x,y1 = handle_composition( "sinc() , poly([2,1])" )
+x,y2 = handle_composition( "  poly( [0, 0.2] )  " )
 write_figure( "sinc_linear", x, y1+y2, xlim, ylim, pure )
 
-x,y1 = fn_poly([0,0.1,0.3,0.2])
-x,y2 = op_compose( [("fn_poly",[0,5]),"fn_sin",("fn_poly",[0,10])] )
+x,y1 = handle_composition( "poly([0,0.1,0.3,0.2]) " )
+x,y2 = handle_composition( "poly([0,5]),sin(),poly([0,10])" )
 write_figure( "sin_poly", x, y1+y2, xlim, ylim, pure )
 
-x,y = op_compose( ["fn_exp","fn_sin"] )
+x,y = handle_composition( "exp(),sin()" )
 write_figure( "sine_in_exp", x, y, xlim, ylim, pure )
 
 x = np.arange(-1, 1, 0.01)
-x,y = op_compose( ["fn_sin",("fn_poly",[0,20]),"fn_exp"], x )
+x,y = handle_composition( "sin(), poly([0,20]) , exp()", x=x )
 write_figure( "exp_in_sine", x, y, xlim, ylim, pure )
 
 x = np.arange(-1, 1, 0.01)
-x,y1 = op_compose( ["fn_sin",("fn_poly",[0,20]),"fn_exp"], x )
-x,y2 = fn_poly([0,0.1,0.3,0.5],x=x)
+x,y1 = handle_composition( "sin(),poly([0,20]),exp()", x=x )
+x,y2 = handle_composition( "poly([0,0.1,0.3,0.5])", x=x )
 write_figure( "exp_in_sine_on_poly", x, y1+y2, xlim, ylim, pure )
 
 x = np.arange(-1, 1, 0.01)
-x,y = op_compose( [("fn_poly",[0,0.1,0.3,0.5]),"fn_sin",("fn_poly",[0,20]),"fn_exp"], x )
+x,y = handle_composition( "poly([0,0.1,0.3,0.5]),sin(),poly([0,20]),exp ()", x=x )
 write_figure( "exp_in_sine_in_poly", x, y, xlim, ylim, pure )
 
-x,y = op_compose( ["fn_ln","fn_sin"] )
+x,y = handle_composition( "ln(),sin()" )
 write_figure( "sin_in_ln", x, y, xlim, ylim, pure )
