6c260055b61b01d307c4d96fdada5b13742ff96f
Konstantinos Chaldaiopoulos <kostaschaldaio@gmail.com>
2023-06-05 23:25:54 +0300

    Update Train_generator.py
    Added multiple exceptions

--- math_expr/Train_generator.py
+++ math_expr/Train_generator.py
@@ -8,7 +8,7 @@ mapping = {
     "6": "0", "7": "0.2", "8": "0.4", "9": "0.6", "10": "0.8", "11": "1.0"
 }
 
-poly_params = [list(itertools.product(range(6, 11), repeat=r)) for r in range(1,5)] # change this for max poly number. If last is 5 than max poly parameters is 4
+poly_params = [list(itertools.product(range(6, 12), repeat=r)) for r in range(1,5)] # change this for max poly number. If last is 5 than max poly parameters is 4
 poly_params = [item for sublist in poly_params for item in sublist]
 
 function_permutations = list(itertools.permutations(range(1, 6))) # generates all possible orderings of the numbers 1 to 5 in order to always use a function from 1 to 5 if possible
@@ -21,24 +21,35 @@ for func_perm in function_permutations:
         for func_key in extended_func_perm:
             tokens.append(str(func_key))
             if func_key == 5:
+                if all([p == '6' for p in poly]):
+                    continue
                 expression.append(f'{mapping[str(func_key)]}({json.dumps(list(map(lambda x: float(mapping[str(x)]), poly)))})')
                 tokens.extend([str(p) for p in poly])
             else:
                 expression.append(f'{mapping[str(func_key)]}()')
-        
+        # Exceptions
+        func_str_list = [mapping[str(func_key)] for func_key in extended_func_perm]
+        exp_ln_undesired = "exp" in func_str_list and "ln" in func_str_list and abs(func_str_list.index("exp") - func_str_list.index("ln")) == 1
+        sin_sinc_undesired = "sin" in func_str_list and "sinc" in func_str_list and abs(func_str_list.index("sin") - func_str_list.index("sinc")) == 1
+        ln_undesired = any(func_str_list[i] == func_str_list[i+1] == 'ln' for i in range(len(func_str_list)-1))
+        if exp_ln_undesired or sin_sinc_undesired or ln_undesired:
+            continue
+            
         expr_str = ", ".join(expression)
         if expr_str not in expressions_set: # check for duplicate expressions
             expressions.append(expr_str)
             expressions_set.add(expr_str)
             tokens_list.append(tokens)
-
-for i in range(100):
-    print(f"{expressions[i]}")
-    print(f"{tokens_list[i]}")
-    print("----------------")
-for i in range(100):  
-    x, y = execute_program_line(expressions[i])
-    write_figure(tokens_list[i], x, y, [-10,10], [-10,10], pure = True) # simply change to expressions[i] if you want the name of the file to be the name of the expression
+n = 1000
+for i in range(min(n, len(expressions))):  # Create a dataset of n images
+    try:
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            x, y = execute_program_line(expressions[i])
+        write_figure(tokens_list[i], x, y, [-10,10], [-10,10], pure=True)  # simply change tokens_list[i] to expressions[i] if you want the name of the file to be the name of the expression
+    except RuntimeWarning:
+        print(f"Skipping expression: {expressions[i]} due to RuntimeWarning")
+        continue
 
 # Can be used to turn back from tokens to expressions
 
